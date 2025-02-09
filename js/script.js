499b6f04440e72a8413a964020363c5ec8bbe221
Chevey-o <cheveyobramble0@gmail.com>
2025-02-09 01:17:15 -0600

    -

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const config = {
     serverInfo: {
         serverLogoImageFileName: "logo.png", /*This is a file name for logo in /images/ (If you upload new logo with other name, you must change this value)*/
         serverName: "The Glade MC", /*Server name*/
-        serverIp: "TheGladeMC.us", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
+        serverIp: "play.theglademc.us", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
         discordServerID: "1334550553954619464" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
     },
 
